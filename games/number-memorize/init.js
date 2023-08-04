54382b0b6a66956299d5d0f5a67ee619423558b6
Ulu-pro <web.ulu7@gmail.com>
2023-08-04 22:13:22 +0500

    Game has been replaced by Matrix memorize

--- games/number-memorize/init.js
+++
@@ -1,9 +0,0 @@
-let numbers = document.querySelectorAll("#numbers td")
-let button = document.querySelector("#hide-button")
-let find = document.querySelector("#find-number")
-let space = document.querySelector("#empty-space")
-let guess = document.querySelector("#guess")
-
-function getRandom() {
-  return Math.floor(Math.random() * 9 + 1).toString()
-}
